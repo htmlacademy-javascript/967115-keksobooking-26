01201ad00fbbd7cc522cabd7c6aa274ed76a86d4
Keks <keks@htmlacademy.ru>
2022-07-11 22:22:47 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/main.js
+++ 5/js/main.js
@@ -1,1 +1,7 @@
-import './adv-gen.js';
+import {generateCard} from './adv-gen.js';
+import {createAdvertisements} from './data-gen.js';
+
+const similarAdvertisementsData = createAdvertisements();
+
+const mapCanvas = document.querySelector('#map-canvas');
+mapCanvas.appendChild(generateCard(similarAdvertisementsData[0]));
