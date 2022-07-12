c41e1ac8091b6394afdb2a7f1c2a6c33a7933f11
idKoala <komachenko.a@gmail.com>
2022-07-12 16:12:42 +0300

    Добавлен модуль form с функциями активации/деактивации форм

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 import {generateCard} from './card.js';
 import {createAdvertisements} from './data-gen.js';
+import {turnActiveModeOff, turnActiveModeOn} from './form.js';
 
 const similarAdvertisementsData = createAdvertisements();
 
 const mapCanvas = document.querySelector('#map-canvas');
 mapCanvas.appendChild(generateCard(similarAdvertisementsData[0]));
+
+turnActiveModeOn();
