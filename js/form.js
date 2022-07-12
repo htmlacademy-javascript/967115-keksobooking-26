e858d87a2e4414701b3ad1b902d438a8b5fa11bc
idKoala <komachenko.a@gmail.com>
2022-07-12 23:08:52 +0300

    Функции активации/деактивации схлопнуты в одну

--- js/form.js
+++ js/form.js
@@ -4,28 +4,18 @@ const filtersForm = document.querySelector('.map__filters');
 const filtersFormSelects = filtersForm.querySelectorAll('select'); // Может лучше через childNode собрать потомков первого уровня?
 const filtersFormFeatures = filtersForm.querySelector('fieldset'); //
 
-function turnActiveModeOn () {
-  advertisementForm.classList.remove('ad-form--disabled');
-  advertisementFields.forEach((advertisementField) => {
-    advertisementField.disabled = false;
-  });
-  filtersForm.classList.remove('ad-form--disabled');
-  filtersFormSelects.forEach((filtersFormSelect) => {
-    filtersFormSelect.disabled = false;
-  });
-  filtersFormFeatures.disabled = false;
+// Блин, классное схлапывание. В копилку
+
+function toggleActiveMode (isActive) {
+    advertisementForm.classList.toggle('ad-form--disabled', isActive);
+    advertisementFields.forEach((advertisementField) => {
+        advertisementField.disabled = isActive;
+    });
+    filtersForm.classList.toggle('ad-form--disabled', isActive);
+    filtersFormSelects.forEach((filtersFormSelect) => {
+        filtersFormSelect.disabled = isActive;
+    });
+    filtersFormFeatures.disabled = isActive;
 }
 
-function turnActiveModeOff () {
-  advertisementForm.classList.add('ad-form--disabled');
-  advertisementFields.forEach((advertisementField) => {
-    advertisementField.disabled = true;
-  });
-  filtersForm.classList.add('ad-form--disabled');
-  filtersFormSelects.forEach((filtersFormSelect) => {
-    filtersFormSelect.disabled = true;
-  });
-  filtersFormFeatures.disabled = true;
-}
-
-export {turnActiveModeOff, turnActiveModeOn};
+export {toggleActiveMode}
--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 import {generateCard} from './card.js';
 import {createAdvertisements} from './data-gen.js';
-import {turnActiveModeOff, turnActiveModeOn} from './form.js';
+import {toggleActiveMode} from './form.js';
 
 const similarAdvertisementsData = createAdvertisements();
 
 const mapCanvas = document.querySelector('#map-canvas');
 mapCanvas.appendChild(generateCard(similarAdvertisementsData[0]));
 
-turnActiveModeOn();
-turnActiveModeOff();
+toggleActiveMode(false);
